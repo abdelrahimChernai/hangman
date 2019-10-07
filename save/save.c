6720f7afb8df13cdcbf04aee06781c1a6e71396c
abdelrahimChernai <himouwiner@gmail.com>
2019-10-07 01:20:14 +0100

    added some comments

--- save/save.c
+++ save/save.c
@@ -6,11 +6,11 @@
 
 void profileInit (PlInf *player, char* saveFileName) {				//creats a player profile or giting access to it
 
-	FILE* saveFile = NULL;
+	FILE* saveFile = NULL;		//the pointer that contains the saving file
 
-	saveFile = fopen (saveFileName, "r+");
-	
-	if (saveFile != NULL) {
+	saveFile = fopen (saveFileName, "r+");		//assining the saving file
+
+	if (saveFile != NULL) {		// checking if the file is opened 
 		do {					//checing the length of the string
 			printf("\n\t\t\tEnter your pseudonym has to have 25 or less characters\n");     //geting the player pseudonym
 			printf("\t\t\t\t    ");scanf("%s", &player->pseudonym);printf("\n");
